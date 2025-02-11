477f95b5e588fcd8dc5522ab2931a83f9682955a
Levent <aydoganlevent@icloud.com>
2025-02-11 12:30:44 +0100

    defined what tobias is

--- FridgeAdministration/ContentView.swift
+++ FridgeAdministration/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Tobias ist n Hurensohn")
         }
         .padding()
     }
