e313e1169d12d7c65bf41f1a3816e787131d6792
PH-apple <harmpau22@bknrd.schule>
2025-02-11 12:55:20 +0100

    haha

--- FridgeAdministration/ContentView.swift
+++ FridgeAdministration/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Tobias ist doppel Hurensohn")
+            Text("Tobias ist cringe")
         }
         .padding()
     }
